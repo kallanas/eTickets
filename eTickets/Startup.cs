8df080f13541960ace0158741a400eb7505d9169
kalliopi <cal.anastasiadou@outlook.com>
2022-01-10 15:27:02 +0200

    CRUD operations and functionality of Actors

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,8 @@ namespace eTickets
             services.AddDbContext<AppDbContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString"))
             );
+
+            services.AddScoped<IActorsService, ActorsService>();
 
             services.AddControllersWithViews();
         }
