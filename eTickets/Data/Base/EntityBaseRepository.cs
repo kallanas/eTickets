65928fc060998f2293e7d1df0b59bbf5e488af33
kalliopi <cal.anastasiadou@outlook.com>
2022-01-10 19:06:46 +0200

    Producers CRUD operations

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -32,7 +32,10 @@ namespace eTickets.Data.Base
             await _context.SaveChangesAsync();
         }
 
-        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
         public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(a => a.Id == id);
 
         public async Task UpdateAsync(int id, T entity)
--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eTickets.Data.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,15 +7,19 @@ using System.Threading.Tasks;
 
 namespace eTickets.Models
 {
-    public class Producer
+    public class Producer : IEntityBase
     {
         [Key]
         public int Id { get; set; }
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage ="The Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
+        [Required(ErrorMessage = "The Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "The full name must be between 3 and 50 characters.")]
         [Display(Name = "Full Name")]
         public string FullName { get; set; }
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "The Bio is required")]
         public string  Bio { get; set; }
 
         //RelationShips
