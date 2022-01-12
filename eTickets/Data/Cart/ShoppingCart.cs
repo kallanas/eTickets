c39d9acd7ab5403c1ec283e283c8c76b95cc1aa9
kalliopi <cal.anastasiadou@outlook.com>
2022-01-12 19:41:38 +0200

    Complete the shoppingCart and the Orders functionality

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,104 @@
+﻿using eTickets.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        private readonly AppDbContext _context;
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public static ShoppingCart GetShoppingCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            var context = services.GetService<AppDbContext>();
+
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddItemToCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(s => s.ShoppingCartId == ShoppingCartId && s.Movie.Id == movie.Id);
+
+            if (shoppingCartItem == default)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    AmountOfMovies = 1
+                };
+
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.AmountOfMovies++;
+                _context.ShoppingCartItems.Update(shoppingCartItem);
+            }
+
+            _context.SaveChanges();
+
+        }
+
+        public void RemoveItemFromCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(s => s.ShoppingCartId == ShoppingCartId && s.Movie.Id == movie.Id);
+
+            if (shoppingCartItem != default)
+            {
+                if (shoppingCartItem.AmountOfMovies > 1)
+                {
+                    shoppingCartItem.AmountOfMovies--;
+                    _context.ShoppingCartItems.Update(shoppingCartItem);
+                }
+                else
+                {
+                    _context.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+            
+            _context.SaveChanges();
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems
+                .Where(s => s.ShoppingCartId == ShoppingCartId)
+                .Include(s => s.Movie)
+                .ToList());
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            var total = _context.ShoppingCartItems
+                .Where(s => s.ShoppingCartId == ShoppingCartId)
+                .Select(s => s.Movie.Price * s.AmountOfMovies).Sum();
+
+            return total;
+        }
+
+        public async Task ClearShoppingCartAsync()
+        {
+            var items = await _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).ToListAsync();
+            _context.ShoppingCartItems.RemoveRange(items);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
